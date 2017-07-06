d0a2f22aeb7624e175f4664968a53e090dfc4120
shariff25 <shariff25@gmail.com>
2017-07-05 23:39:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def makeWebhookResult(req):
 
 if __name__ == '__main__':
         port = int(os.getenv('PORT', 5000))
-        app.run(host='127.0.0.1', port=5000)
+        app.run(host='0.0.0.0', port=5000)
