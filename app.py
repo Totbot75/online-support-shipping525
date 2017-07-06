12b6ebd410cbe8b76864d58bbcc825462b93bd1a
shariff25 <shariff25@gmail.com>
2017-07-06 14:24:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import urllib
 import json
 import os
 
-
 from flask import Flask
 from flask import request
 from flask import make_response
@@ -35,7 +34,7 @@ def makeWebhookResult(req):
     parameters = result.get("parameters")
     zone = parameters.get("shipping-zone")
 
-    cost = {'Euro':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
     speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
 
@@ -50,9 +49,10 @@ def makeWebhookResult(req):
         "source": "apiai-onlinestore-shipping"
     }
 
+
 if __name__ == '__main__':
-          port = int(os.getenv('PORT', 5000))
-          app.run(debug=True, port=port, host='0.0.0.0')
-          app.run(host='0.0.0.0', port=5000)
-        
-  
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+    app.run(debug=True, port=port, host='0.0.0.0')
